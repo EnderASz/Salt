7253b421150903ac8844c37e050f81c3ee629533
bellrise <bellrise.av@gmail.com>
2021-03-05 14:07:06 +0100

    Add comments to saltc.cpp

--- saltc/saltc.cpp
+++ saltc/saltc.cpp
@@ -1,13 +1,21 @@
 /**
- * Core Salt compiler file. 
+ * Core Salt compiler file. Takes care of the arguments and runs the core
+ * parts.
  *
- *
+ * @author bellrise
  */
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
 #include <string>
 
+#include "include/utils.h"
+
+using std::string;
+
+/* Print the help page and exit the program. This will probably be moved to
+ * a help.txt file later on in development.
+ */
 void core_print_help()
 {
     printf("%s\n\n%s\n%s\n", "Usage: saltc [OPTIONS]... FILE", 
@@ -16,6 +24,10 @@ void core_print_help()
     exit(0);
 }
 
+/* This is the main function for the salt compiler. It takes care of the 
+ * arguments and controls the core parts of this program, like the parser,
+ * tokenizer, validator and synthesizer.
+ */
 int main(int argc, char* argv[])
 {
     if (argc < 2) {
@@ -24,13 +36,13 @@ int main(int argc, char* argv[])
     }
 
     // Load args
-    std::vector<std::string> args;
+    std::vector<string> args;
     for (uint i = 0; i < argc; i++) {
         args.push_back(argv[i]);
     }
 
     // Find --help or -h
-    for (std::string& arg : args) {
+    for (string& arg : args) {
         if ((arg == "--help") || (arg == "-h"))
             core_print_help();
     }
