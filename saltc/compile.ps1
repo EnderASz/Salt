8f237dacbdc6123cc93ae144503ad5a01ad03c64
EnderASz <olek5188@gmail.com>
2021-03-06 18:40:22 +0100

    Fix saltc compile script for Windows problems with: - variables naming conflicts - generating arguments for main source compilation - returning right path of result binary file

--- saltc/compile.ps1
+++ saltc/compile.ps1
@@ -1,52 +1,53 @@
 # Powershell compilation script for the Salt Compiler for Windows system.
 
 # region - Setup before compilation
-    $MAIN = "saltc.cpp"
-    $RESULT = "saltc.exe"
 
     # Set all needed paths paths and directories
     $BASEDIR = $PSScriptRoot
+    $MAIN_SOURCE_PATH = "$BASEDIR\saltc.cpp"
+    $RESULT_FILENAME = "saltc.exe"
+
     $BuildDir = "$BASEDIR\build"
     $SourcesDir = "$BASEDIR\src"
     $CompiledObjectsDir = "$BuildDir\objects"
-    $CompiledMainPath = "$BuildDir\$RESULT"
+    $BuildedMainPath = "$BuildDir\$RESULT_FILENAME"
 
     # Create all needed directories
     New-Item -ErrorAction Ignore -Path $CompiledObjectsDir -ItemType "directory"
 
     # Search all sources to build object files
     $CPPSources = Get-ChildItem -Path $SourcesDir -Include *.cpp -Recurse
-    $CPPSourcesNames = ($CPP_SOURCES | %{$_.Basename})
-    $CPPSourcesPaths = ($CPP_SOURCES | %{$_.FullName})
+    $CPPSourcesNames = ($CPPSources | %{$_.Basename})
+    $CPPSourcesPaths = ($CPPSources | %{$_.FullName})
     $CSources = Get-ChildItem -Path $SourcesDir -Include *.c -Recurse
-    $CSourcesNames = ($C_SOURCES | %{$_.Basename})
-    $CSourcesPaths = ($C_SOURCES | %{$_.FullName})
+    $CSourcesNames = ($CSources | %{$_.Basename})
+    $CSourcesPaths = ($CSources | %{$_.FullName})
 
     # Set paths for future object files
-    $CPP_OBJECTS_PATHS = ($CPP_SOURCES_NAMES | %{@("$CompiledObjectsDir\$_.o")})
-    $C_OBJECTS_PATHS = ($C_SOURCES_NAMES | %{@("$CompiledObjectsDir\$_.o")})
-    $ALL_OBJECTS_PATHS = $CPP_OBJECTS_PATHS + $C_OBJECTS_PATHS
+    $CPPObjectsPaths = ($CPPSourcesNames | %{@("$CompiledObjectsDir\$_.o")})
+    $CObjectsPaths = ($CSources | %{@("$CompiledObjectsDir\$_.o")})
+    $AllObjectsPaths = $CPPObjectsPaths + $CObjectsPaths
 # endregion
 
 # region - Compilation
     # Compile all sources to object files
-    For($i = 0; $i -lt $CPP_OBJECTS_PATHS.count; $i++) {
-        g++ -c -o $CPP_OBJECTS_PATHS[$i] $CPP_SOURCES_PATHS[$i]
+    For($i = 0; $i -lt $CPPObjectsPaths.count; $i++) {
+        g++ -c -o $CPPObjectsPaths[$i] $CPPSourcesPaths[$i]
     }
-    For($i = 0; $i -lt $C_SOURCES_NAMES.count; $i++) {
-        gcc -c -o $C_OBJECTS_PATHS[$i] $C_SOURCES_PATHS[$i]
+    For($i = 0; $i -lt $CSourcesPaths.count; $i++) {
+        gcc -c -o $CObjectsPaths[$i] $CSourcesPaths[$i]
     }
 
     # Compile the main file and link with object files
-    g++ -o $CompiledMainPath $MAIN ($ALL_OBJECTS_PATHS -join ' ')
+    g++ -o $BuildedMainPath $MAIN_SOURCE_PATH $AllObjectsPaths
 # endregion
 
 # region - Cleaning after compilation
     # Move the compiled result to the top folder
-    Move-Item -Path $CompiledMainPath -Destination $BASEDIR -Force
+    Move-Item -Path $BuildedMainPath -Destination $BASEDIR -Force
 
     # Remove compile-time directories
     Remove-Item -Path $BuildDir -Recurse -Force
 # endregion
 
-return $CompiledMainPath
+return "$BASEDIR\$RESULT_FILENAME"
