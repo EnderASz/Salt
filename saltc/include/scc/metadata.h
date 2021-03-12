fd5c0af37862d0e53c1f0d34f49be5c1f370e1ae
EnderASz <46653720+EnderASz@users.noreply.github.com>
2021-03-12 11:12:12 +0100

    saltc: Init metadata class in new namespace salt::scc

---
+++ saltc/include/scc/metadata.h
@@ -0,0 +1,35 @@
+/**
+ * The Metadata static class contains metadata
+ * about compiler, language and program.
+ */
+#ifndef META_H_
+#define META_H_
+
+#include "../utils.h"
+
+#include <string>
+using std::string;
+
+namespace salt::scc
+{
+    class Metadata
+    {
+    private:
+        /* Magic string, indicates that is indeed a SCC file. */
+        static const std::array<byte, 6> SCC_HEADER;
+
+        /* Format version identificator */
+        static const std::array<byte, 2> SCC_VERSION;
+
+        /* Compiler signature */
+        static const std::array<byte, 8> COMPILER_SIGNATURE;
+
+        int instructions_amount;
+
+        int string_literals_amount;
+
+    };
+
+};
+
+#endif // META_H_
