50c0fed9dbf40f89315b79cd82cf4efc8c20b180
EnderASz <46653720+EnderASz@users.noreply.github.com>
2021-03-16 19:27:46 +0100

    saltc: Rename Metadata class to CompilerMetadata saltc: Change static consts access to public saltc: Remove getter for static consts saltc: Remove metadata about code members

---
+++ saltc/src/compiler_metadata.cpp
@@ -0,0 +1,25 @@
+/**
+ * meta.h implementation
+ * 
+ */
+#include "../include/compiler_metadata.h"
+
+#include "../include/utils.h"
+#include <array>
+
+namespace salt
+{
+
+    /* Magic string, indicates that is indeed a SCC file. "\x7fSCC\xFF\xEE"*/
+    const std::array<byte, 6> CompilerMetadata::SCC_HEADER = {
+        '\x7F', 'S', 'C', 'C', '\xFF', '\xEE'};
+
+    /* Format version identificator */
+    const std::array<byte, 2> CompilerMetadata::SCC_VERSION = {'\x01', '\x00'};
+
+    /* Compiler signature */
+    const std::array<byte, 8> CompilerMetadata::COMPILER_SIGNATURE = {
+        '\x7f','\x7a','\x7b','\x7c','\x20','\x10','\x02','\x0a'};
+        /*                             xx     xx     xx         */
+
+}
