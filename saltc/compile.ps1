1142503589ac8647faa8ddd3e90e5b0296cea230
EnderASz <46653720+EnderASz@users.noreply.github.com>
2021-03-22 12:36:26 +0100

    saltc: Add -Debug flag to compilation Windows Powershell script saltc: Add logging.h contains macro for debug printing

--- saltc/compile.ps1
+++ saltc/compile.ps1
@@ -2,13 +2,21 @@
 
 param (
     [Parameter()]
-    [switch]$AllWarnings = $false
+    [switch]$AllWarnings = $false,
+
+    [Parameter()]
+    [switch]$Debug = $false
 )
 
 if($AllWarnings) {
     $wall_flag = "-Wall"
 } else {
     $wall_flag = ""
+}
+if($Debug) {
+    $debug_flag = "-D DEBUG"
+} else {
+    $debug_flag = ""
 }
 
 # region - Setup before compilation
@@ -40,11 +48,11 @@ if($AllWarnings) {
     For($i = 0; $i -lt $CPPObjectsPaths.count; $i++) {
         $CPPObjectPath = $CPPObjectsPaths[$i]
         $CPPSourcePath = $CPPSourcesPaths[$i]
-        g++ -std="c++1z" $wall_flag -c -o $CPPObjectPath $CPPSourcePath
+        g++ -std="c++1z" $debug_flag $wall_flag -c -o $CPPObjectPath $CPPSourcePath
     }
 
     # Compile the main file and link with object files
-    g++ -std="c++1z" $wall_flag -o $BuildedMainPath $MAIN_SOURCE_PATH $CPPObjectsPaths
+    g++ -std="c++1z" $debug_flag $wall_flag -o $BuildedMainPath $MAIN_SOURCE_PATH $CPPObjectsPaths
 # endregion
 
 # region - Cleaning after compilation
