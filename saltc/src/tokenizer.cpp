f628e7d32d0befd4e99d02bb449b0d4fd08bc878
EnderASz <46653720+EnderASz@users.noreply.github.com>
2021-03-22 17:21:02 +0100

    saltc: Extend Tokenizer class and render method > Replace code string with source file instance + Add skipping comments + Add skipping blank spaces + Add sourceBegin/End/Last methods + Add findFirst method + Add isCommentChar method + Add isLastChar method + Add isInRange method + Add jumpToEnd method

--- saltc/src/tokenizer.cpp
+++ saltc/src/tokenizer.cpp
@@ -4,21 +4,26 @@
  */
 #include "../include/tokenizer.h"
 
+#include "../include/source_file.h"
+#include "../include/utils.h"
+#include "../include/errors/unclosed_comment_error.h"
 #include <vector>
+#include <cstring>
+#include <algorithm>
 
 using std::string;
+using std::find;
+using std::distance;
 
 namespace salt::tokenizer
 {
 
 /**
  * Tokenzizer constructor. A new instance is supposed to be created for
- * every new string that should be parsed. After initializing the object,
+ * source file instance. After initializing the object,
  * you can then call Tokenizer::render().
  */
-Tokenizer::Tokenizer(string content) {
-    this->original = content;
-}
+Tokenizer::Tokenizer(SourceFile& source_file): source(&source_file) {}
 
 /**
  * Returns the array of tokens in a list object. The list object is just 
@@ -26,9 +31,67 @@ Tokenizer::Tokenizer(string content) {
  * uses a std::vector<Token> or a Token*, depending on the implementation.
  */
 std::vector<Token> Tokenizer::render() {
-    std::vector<Token> tokens;
+    current = sourceBegin();
+
+    while(isInRange(current)) {
+        skipComment();
+        skipBlank();
+        if(!isInRange()) break;
+        if(isLastChar()) {
+            // Do stuff only for last char
+            break;
+        }
+        // Do stuff only for character from begin to last-1
+    }
     
     return tokens;
+}
+
+void Tokenizer::skipComment() {
+    while(isInRange() && isCommentChar()) {
+        if(isLastChar()) jumpToEnd();
+        if(*(current+1) == '[') {
+            string::iterator close = findFirst("]#");
+            if(!isInRange(close))
+                throw errors::UnclosedCommentError(
+                    InStringPosition(source->code, current),
+                    *source);
+            current = close+2;
+        }
+        else current = findFirst("\n")+1;
+    }
+}
+
+void Tokenizer::skipBlank(){
+     while(isInRange() && isblank(*current)) current += 1;
+}
+
+void Tokenizer::jumpToEnd() {current = sourceEnd();}
+
+string::iterator Tokenizer::sourceBegin() const {return source->code.begin();}
+
+string::iterator Tokenizer::sourceEnd() const {return source->code.end();}
+
+string::iterator Tokenizer::sourceLast() const {return source->code.end()-1;}
+
+string::iterator Tokenizer::findFirst(string value) const {
+    return find(current, sourceEnd(), value);
+}
+
+bool Tokenizer::isCommentChar() const {return isCommentChar(current);}
+bool Tokenizer::isCommentChar(string::iterator iterator) const {
+    return isCommentChar(*iterator);
+}
+bool Tokenizer::isCommentChar(const char chr) const {return chr == '#';}
+
+bool Tokenizer::isLastChar() const {return isLastChar(current);}
+bool Tokenizer::isLastChar(string::iterator iterator) const {
+    return distance(iterator, sourceEnd()-1) == 0;
+}
+
+bool Tokenizer::isInRange() const {return isInRange(current);}
+bool Tokenizer::isInRange(string::iterator iterator) const {
+    return distance(iterator, sourceEnd()-1) >= 0;
 }
 
 } // salt::tokenizer
