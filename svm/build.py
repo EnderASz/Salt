0b14b062ab3cbff3f9e80698314c1223379b8002
bellrise <65973858+bellrise@users.noreply.github.com>
2021-03-31 22:08:57 +0200

    svm: rewrite Makefile
    The SVM Makefile had a major rewrite, making it more clean and floofy!
    Comments are also included.

--- svm/build.py
+++ svm/build.py
@@ -1,19 +1,27 @@
-# Fetch information from the dev.json file
-
-import json
+# Fetch information from the version.conf file.
+# It is guaranteed to find the major, minor and build
+# fields in the configuration.
 import sys
 
-if len(sys.argv) < 2:
-    print('[!] Please provide argument in build.py')
-    exit(1)
+with open(sys.argv[1]) as f:
+    file = f.readlines()
+
+info = {}
+
+for i in file:
+    if i.strip() and not i.startswith('#'):
+        strings = [s.strip() for s in i.split('=')]
+        info[strings[0]] = strings[1]
+
+print(f"{info['major']}.{info['minor']}\\ build\\ {info['build']}")
+
+# Bump build
+info['build'] = str(int(info['build']) + 1)
+
+# Write the result back to the file with the bumped build version
+with open(sys.argv[1], 'w') as f:
+    f.write('# The version file for SVM builds. Used in the Makefile\n\n')
+    for k, v in info.items():
+        f.write(f'{k}={v}\n')
 
 
-with open('dev.json') as f:
-    dev = json.load(f)
-
-if sys.argv[1] == 'build':
-    dev['build'] += 1
-    print(f"svm\\ {dev['major']}.{dev['minor']}\\ build\\ {dev['build']}")
-
-with open('dev.json', 'w') as f:
-    json.dump(dev, f, indent=4)
