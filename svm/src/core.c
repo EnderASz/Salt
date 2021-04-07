90bf183408790dbea7d863cb7db0654dfc3d0674
bellrise <bellrise.av@gmail.com>
2021-04-07 19:32:49 +0200

    svm: add licence text to all .c source files

--- svm/src/core.c
+++ svm/src/core.c
@@ -1,4 +1,30 @@
 /**
+ * Salt Virtual Machine
+ * 
+ * Copyright (C) 2021  The Salt Programming Language Developers
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * END OF COPYRIGHT NOTICE
+ *
+ * The Salt Virtual Machine is the interpreter for compiled Salt code generated
+ * by saltc, the Salt compiler. It is written in C to have more control over 
+ * the bytes and what is happening in the background, to achieve better 
+ * execution speeds. This code is mostly written and handled by me (bellrise)
+ * but there may be more people in the future wanting to contribute to the
+ * project. 
+ *
  * core.h implementation
  *
  * @author bellrise, 2021
