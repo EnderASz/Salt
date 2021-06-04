0bec5de6e421b5a4c69f4a7c232f50214692182e
EnderASz <olek5188@gmail.com>
2021-06-04 13:42:12 +0200

    Add prints describing what compile script currently doing Replaced some `iex` calls to `Invoke-Expression`

--- saltc/compile.ps1
+++ saltc/compile.ps1
@@ -20,6 +20,7 @@ if($SDebug) {
 # region - Setup before compilation
 
     # Set all needed paths paths and directories
+    Write-Host "Setting up and creating needed paths and directories..."
     $BASEDIR = $PSScriptRoot
     $MAIN_SOURCE_PATH = "$BASEDIR\saltc.cpp"
     $RESULT_FILENAME = "saltc.exe"
@@ -30,9 +31,10 @@ if($SDebug) {
     $BuildedMainPath = "$BuildDir\$RESULT_FILENAME"
 
     # Create all needed directories
-    New-Item -ErrorAction Ignore -Path $CompiledObjectsDir -ItemType "directory"
+    New-Item -ErrorAction Ignore -Path $CompiledObjectsDir -ItemType "directory" | Out-Null
 
     # Search all sources to build object files
+    Write-Host "Searching all sources files..."
     $CPPSources = Get-ChildItem -Path $SourcesDir -Include *.cpp -Recurse
     $CPPSourcesNames = ($CPPSources | %{$_.Basename})
     $CPPSourcesPaths = ($CPPSources | %{$_.FullName})
@@ -46,13 +48,15 @@ if($SDebug) {
     For($i = 0; $i -lt $CPPObjectsPaths.count; $i++) {
         $CPPObject = $CPPObjectsPaths[$i]
         $CPPSource = $CPPSourcesPaths[$i]
+        Write-Host "Compiling '$CPPSource' source file..."
         $__FILENAME__ = ($CPPSource | Resolve-Path -Relative).substring(2).replace("\", "/")
-        iex "g++ $static_flags -c -o $CPPObject $CPPSource -D __FILENAME__=`'\`"$__FILENAME__\`"`'"
+        Invoke-Expression "g++ $static_flags -c -o $CPPObject $CPPSource -D __FILENAME__=`'\`"$__FILENAME__\`"`'"
     }
 
     # Compile the main file and link with object files
+    Write-Host "Compiling main file and linking everything needed to executable file..."
     $__FILENAME__ = ($MAIN_SOURCE_PATH | Resolve-Path -Relative).substring(2).replace("\", "/")
-    iex "g++ $static_flags -o $BuildedMainPath $MAIN_SOURCE_PATH $CPPObjectsPaths -D __FILENAME__=`'\`"$__FILENAME__\`"`'"
+    Invoke-Expression "g++ $static_flags -o $BuildedMainPath $MAIN_SOURCE_PATH $CPPObjectsPaths -D __FILENAME__=`'\`"$__FILENAME__\`"`'"
 # endregion
 
 # region - Cleaning after compilation
@@ -62,5 +66,5 @@ if($SDebug) {
     # Remove compile-time directories
     Remove-Item -Path $BuildDir -Recurse -Force
 # endregion
-
+Write-Host "Executable saved at: " -NoNewline
 return "$BASEDIR\$RESULT_FILENAME"
